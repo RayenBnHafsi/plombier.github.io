1d88d4e4b2c63adfdea0a163205c65c30eb2f041
RayenBnHafsi <120422156+RayenBnHafsi@users.noreply.github.com>
2023-01-19 16:28:37 +0100

    setting update

--- project_managment/settings.py
+++ project_managment/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ke!oa9f&)*f+$rip)ez9cifuu+k6ni-^mta$rb&$-+$5a9b1bb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rayenbnhafsi.github.io/plombier.github.io']
 
 
 # Application definition
